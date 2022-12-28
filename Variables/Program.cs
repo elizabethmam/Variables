1294a877d2d425cf5a77aa4948e42b0536f9cac9
elizabethmam <elizabethguerrero.mam@gmail.com>
2022-12-28 11:24:47 -0600

    first commit

---
+++ Variables/Program.cs
@@ -0,0 +1,30 @@
+﻿// Non-nullable y default
+int edad = default(int);
+Console.WriteLine($"Éste es un entero por defecto: {edad}");
+
+bool variableBooleana = default(bool);
+Console.WriteLine($"Ésta es una variable booleana por defecto: {variableBooleana}");
+
+decimal precio = default(decimal); //base 10 -> valor más preciso y es recomendable
+                                        //para operaciones con dinero.
+Console.WriteLine($"Éste es un décimal por defecto: {precio}");
+
+DateTime fecha = default(DateTime);
+Console.WriteLine($"Ésta es una variable de tipo DateTime por defecto: {fecha}");
+
+//El valor por defecto de un string es null
+string? nombre = null;
+Console.WriteLine($"Éste es un string por defecto: {nombre}");
+//Console.WriteLine(nombre.ToUpper());
+
+// Desbordamiento de memoria (overflow) y cómo evitarla
+int maxValue = int.MaxValue;
+Console.WriteLine(maxValue + 1); //desbordamiento de la memoria
+Console.WriteLine(checked(maxValue + 1)); //advertencia de overflow
+
+checked //advertencia de desbordamiento de memoria
+{
+    int maxValue2 = int.MaxValue;
+    Console.WriteLine(maxValue2 + 1);
+}
+
